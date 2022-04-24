9d67f92357e1aa58a3fbb2283b6fd179093d6805
gaganbiswas <gaganbiswas.me1@gmail.com>
2022-04-24 12:52:13 +0530

    Snippet page

--- pages/snippets.tsx
+++ pages/snippets.tsx
@@ -1,20 +1,50 @@
 import { GetStaticProps, NextPage } from 'next'
 import React from 'react'
+import Layout from '../components/Layout'
+import SnippetCard from '../components/SnippetCard'
 import { sanityClient } from '../sanity'
 
-type Props = {
-  snippets: {
-    _id: string
-    title: string
-    summary: string
-    slug: {
-      current: string
-    }
+type Snippet = {
+  _id: string
+  title: string
+  summary: string
+  logo: string
+  slug: {
+    current: string
   }
 }
 
+type Props = {
+  snippets: [Snippet]
+}
+
 const Snippets: NextPage<Props> = ({ snippets }) => {
-  return <div>snippets</div>
+  return (
+    <Layout>
+      <div className="mb-16 flex max-w-2xl flex-col items-start justify-center">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
+          Code Snippets
+        </h1>
+        <p className="mb-4 text-gray-600 dark:text-gray-400">
+          These are a collection of code snippets I've used in the past and
+          saved. Some are Serverless Functions, which include set up
+          instructions. Others are anything from random CSS snippets to Node.js
+          scripts.
+        </p>
+        <div className="my-2 mt-4 grid w-full grid-cols-1 gap-4 sm:grid-cols-2">
+          {snippets.map((snippet) => (
+            <SnippetCard
+              key={snippet.slug}
+              title={snippet.title}
+              slug={snippet.slug.current}
+              logo={snippet.logo}
+              description={snippet.summary}
+            />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Snippets
@@ -24,6 +54,7 @@ export const getStaticProps: GetStaticProps = async () => {
       _id,
       title,
       summary,
+      logo,
       slug {
         current
       },
--- pages/snippets/[slug].tsx
+++ pages/snippets/[slug].tsx
@@ -1,7 +1,10 @@
 import { NextPage } from 'next'
-import { MDXRemoteSerializeResult } from 'next-mdx-remote'
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
+import Image from 'next/image'
 import React from 'react'
+import Layout from '../../components/Layout'
+import MDXComponents from '../../components/MDXComponents'
 import { sanityClient } from '../../sanity'
 
 type Props = {
@@ -10,12 +13,40 @@ type Props = {
     publishedAt: string
     title: string
     summary: string
+    logo: string
   }
   mdxSource: MDXRemoteSerializeResult
 }
 
 const Snippet: NextPage<Props> = ({ snippet, mdxSource }) => {
-  return <div>Snippet</div>
+  return (
+    <Layout>
+      <article className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
+        <div className="mb-8 flex w-full justify-between">
+          <div>
+            <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
+              {snippet.title}
+            </h1>
+            <p className="text-gray-700 dark:text-gray-300">
+              {snippet.summary}
+            </p>
+          </div>
+          <div className="mt-2 sm:mt-0">
+            <Image
+              alt={snippet.title}
+              height={48}
+              width={48}
+              src={`/logos/${snippet.logo}`}
+              className="rounded-full"
+            />
+          </div>
+        </div>
+        <div className="prose w-full dark:prose-dark">
+          <MDXRemote {...mdxSource} components={{ ...MDXComponents }} />
+        </div>
+      </article>
+    </Layout>
+  )
 }
 
 export default Snippet
